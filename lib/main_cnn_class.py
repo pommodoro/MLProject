1a9c7b9a403d83791a2ccb6073851be7f3e762fc
Saboya <hs2923@columbia.edu>
2017-12-11 11:16:05 -0500

    use small batches

--- lib/main_cnn_class.py
+++ lib/main_cnn_class.py
@@ -12,6 +12,7 @@ from __future__ import print_function
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 
 # Import script with auxiliar functions
 import ConvDeconvClassesMnist as nets
@@ -25,11 +26,12 @@ def main(unused_argv):
 
   # Load training and eval data
   mnist        = tf.contrib.learn.datasets.load_dataset("mnist")
-  train_data   = mnist.train.images[0:1000] # Returns np.array - reading only 1000 images
-  train_labels = np.asarray(mnist.train.labels[0:1000], dtype=np.int32) # - reading only 1000 images
-  eval_data    = mnist.test.images[0:200] # Returns np.array - reading only 200 images
-  eval_labels  = np.asarray(mnist.test.labels[0:200], dtype=np.int32) # - reading only 200 images
-
+  train_data   = mnist.train.images # Returns np.array - reading only 1000 images
+  train_labels = np.asarray(mnist.train.labels, dtype=np.int32) # - reading only 1000 images
+  eval_data    = mnist.test.images # Returns np.array - reading only 200 images
+  eval_labels  = np.asarray(mnist.test.labels, dtype=np.int32) # - reading only 200 images
+  
+  batch_size = 50
 
   ############################################
   ## Run session
@@ -52,18 +54,25 @@ def main(unused_argv):
         sess.run(tf.global_variables_initializer())
 
         # make labels one-hot encoded
-        onehot_labels_train = tf.one_hot( indices = tf.cast(train_labels, tf.int32), depth =  10 )
+        onehot_labels_train = tf.one_hot( indices = tf.cast(train_labels, tf.int32), depth =  10 ).eval()
 
         # print error rate before training
         print('error rate BEFORE training is {}'.format((np.sum(net.compute(train_data)!=train_labels) / train_labels.size)))
 
         # train network
-        net.train( train_data, onehot_labels_train.eval() )
-
+        #net.train( train_data, onehot_labels_train )
+ 
         # now train...
-        for i in range(15):
-            net.train(train_data,onehot_labels_train.eval())
-            print('error rate during training is {}'.format(( np.sum(net.compute(train_data)!=train_labels) / train_labels.size)))
+        for i in range(1000):            
+            batch = random.sample(range( train_data.shape[0] ), batch_size)
+        
+            x_batch = train_data[batch,]
+            y_batch = onehot_labels_train[batch,]
+            
+            net.train(x_batch, y_batch)
+            
+            if i % 100 == 0:
+                print('error rate during training is {}'.format(( np.sum(net.compute(train_data)!=train_labels) / train_labels.size)))
         
         # save the trained network 
         net.netSaver("./tmp/cnnMnist")
@@ -96,20 +105,20 @@ def main(unused_argv):
         #plt.imshow(np.array(a1[1,:,:,0]), cmap='gray')
         #plt.show()
 
-        #conv1, conv2 = net.getConvs()
+        conv1, conv2 = net.getConvs()
         #a1 = conv1.eval(feed_dict = {net.x: train_data})
         
         print("\n")
-        a1 = DeconvNet.displayFeatures1(train_data, train_labels)
+        a1 = DeconvNet.displayFeatures1(train_data[:100,], train_labels[:100])
         print(a1.shape)
         
         print("\n")
-        a2 = DeconvNet.displayFeatures2(train_data, train_labels)
+        a2 = DeconvNet.displayFeatures2(train_data[:100,], train_labels[:100])
         print(a2.shape)      
         
-        plt.imshow(np.array(a1[0, 1, :, :, 0]), cmap='gray')
-        plt.imshow(np.array(a2[0, 1, :, :, 0]), cmap='gray')
-        plt.show()
+        #plt.imshow(np.array(a1[0, 1, :, :, 0]), cmap='gray')
+        #plt.imshow(np.array(a2[0, 1, :, :, 0]), cmap='gray')
+        #plt.show()
 
         # plt.imshow(np.array(train_data[1,:,:,0]), cmap='gray')
         # plt.show()
