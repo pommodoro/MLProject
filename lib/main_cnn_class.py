cd629019738d5179acf7fcfbff14bec0556c3eed
Saboya <hs2923@columbia.edu>
2017-12-11 16:33:51 -0500

    Mnist running faster

--- lib/main_cnn_class.py
+++ lib/main_cnn_class.py
@@ -31,7 +31,8 @@ def main(unused_argv):
   eval_data    = mnist.test.images # Returns np.array - reading only 200 images
   eval_labels  = np.asarray(mnist.test.labels, dtype=np.int32) # - reading only 200 images
   
-  batch_size = 50
+  batch_size = 32
+  max_iter = 1000
 
   ############################################
   ## Run session
@@ -56,14 +57,13 @@ def main(unused_argv):
         # make labels one-hot encoded
         onehot_labels_train = tf.one_hot( indices = tf.cast(train_labels, tf.int32), depth =  10 ).eval()
 
-        # print error rate before training
-        print('error rate BEFORE training is {}'.format((np.sum(net.compute(train_data)!=train_labels) / train_labels.size)))
-
+        # print error rate before training        
+        print('Aproximate error rate BEFORE training is {} %'.format(round(np.sum(net.compute(train_data[:1000,])!=train_labels[:1000]) / train_labels[:1000].size *100, 2)))
         # train network
         #net.train( train_data, onehot_labels_train )
  
         # now train...
-        for i in range(1000):            
+        for i in range(max_iter):            
             batch = random.sample(range( train_data.shape[0] ), batch_size)
         
             x_batch = train_data[batch,]
@@ -72,7 +72,11 @@ def main(unused_argv):
             net.train(x_batch, y_batch)
             
             if i % 100 == 0:
-                print('error rate during training is {}'.format(( np.sum(net.compute(train_data)!=train_labels) / train_labels.size)))
+                print('Aproximate error rate during iteration {} is {} %'.format(i, round(np.sum(net.compute(train_data[:1000,])!=train_labels[:1000]) / train_labels[:1000].size *100, 2)))
+            
+        print('Final training error is {} %'.format(round(np.sum(net.compute(train_data)!=train_labels) / train_labels.size *100, 2)))
+        
+        print('Final test error is {} %'.format(round(np.sum(net.compute(eval_data)!=eval_labels) / eval_labels.size *100, 2)))
         
         # save the trained network 
         net.netSaver("./tmp/cnnMnist")
@@ -101,20 +105,22 @@ def main(unused_argv):
         print( dec2.shape )
         print( dec2 )
 
-        #a1 = dec1.eval()
-        #plt.imshow(np.array(a1[1,:,:,0]), cmap='gray')
-        #plt.show()
-
         conv1, conv2 = net.getConvs()
-        #a1 = conv1.eval(feed_dict = {net.x: train_data})
         
         print("\n")
-        a1 = DeconvNet.displayFeatures1(train_data[:100,], train_labels[:100])
+        a1 = DeconvNet.displayFeatures1(eval_data[:100,], eval_labels[:100])
         print(a1.shape)
         
         print("\n")
-        a2 = DeconvNet.displayFeatures2(train_data[:100,], train_labels[:100])
+        a2 = DeconvNet.displayFeatures2(eval_data[:100,], eval_labels[:100])
         print(a2.shape)      
+        
+        np.save("tmp/ActivationsArray_Layer1_Run2.npy", a1)
+        np.save("tmp/ActivationsArray_Layer2_Run2.npy", a2)
+        
+        #a1 = dec1.eval()
+        #plt.imshow(np.array(a1[1,:,:,0]), cmap='gray')
+        #plt.show()
         
         #plt.imshow(np.array(a1[0, 1, :, :, 0]), cmap='gray')
         #plt.imshow(np.array(a2[0, 1, :, :, 0]), cmap='gray')
