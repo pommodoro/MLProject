81cb42695529614a6f3adefa6e833f6de1cce01c
amangulati92 <31743888+amangulati92@users.noreply.github.com>
2017-12-16 17:06:38 -0500

    DMF norm correction and activations

--- lib/main_dataset2.py
+++ lib/main_dataset2.py
@@ -63,7 +63,7 @@ def main(unused_argv):
 #   ### Read resized images and store in np.array
 #   ############################################
 #   #path with resized images
-#   resized_train_path = "data/dataset2/train_resized/"
+#   resized_train_path = "../data/dataset2/train_resized/"
 #   files = [resized_train_path + f for f in os.listdir(resized_train_path) if f.endswith('.jpg')]
 #   images = []
 #   images_flip = []
@@ -122,12 +122,12 @@ def main(unused_argv):
 #   print("notRGB files")
 #   print(notRGB)
 #
-#   #save training data (3 np arrays)
-#   np.save("data/dataset2/train_data_normal.npy", train_normal )
-#   np.save("data/dataset2/train_data_flipped.npy", train_flipped )
-#   np.save("data/dataset2/train_data_rotated.npy", train_rotated )
-#   np.savetxt("data/dataset2/train_data_notRGB.txt", notRGB, fmt='%s')
-
+##   #save training data (3 np arrays)
+#   np.save("../data/dataset2/train_data_normal.npy", train_normal )
+#   np.save("../data/dataset2/train_data_flipped.npy", train_flipped )
+#   np.save("../data/dataset2/train_data_rotated.npy", train_rotated )
+#   np.savetxt("../data/dataset2/train_data_notRGB.txt", notRGB, fmt='%s')
+#
 
   ###
   ### Load data
@@ -135,55 +135,53 @@ def main(unused_argv):
 
   
   # Load images
-  print("Loading images...")
-  train_normal  = np.load("data/dataset2/train_data_normal.npy" )
-  train_flipped = np.load("data/dataset2/train_data_flipped.npy" )
-  train_rotated = np.load("data/dataset2/train_data_rotated.npy" )
+    print("Loading images...")
+    train_normal  = np.load("../data/dataset2/train_data_normal.npy" )
+    train_flipped = np.load("../data/dataset2/train_data_flipped.npy" )
+    train_rotated = np.load("../data/dataset2/train_data_rotated.npy" )
 
   # stacking in a single np.array
-  train_all = np.concatenate( (train_normal, train_flipped, train_rotated) )
+    train_all = np.concatenate( (train_normal, train_flipped, train_rotated) )
 
-  # normalize
-  print("Normalizing images...")
-  train_all = train_all[ None:, :, :, :,] * (1.0 / 255.0)
-
+  # normaliz
+    print("Normalizing images...")
+    train_all = train_all[ None:, :, :, :,] * (1.0 / 255.0)
+    
   # normalize all values 
-  # train_all = np.multiply( train_all[0:0,:,:,:], 1/255.0 )
-
+    #train_all = np.multiply( train_all[0:0,:,:,:], 1/255.0 )
+    #print(train_all.shape)
   # Load file names that are not RGB (to be excluded from train data)
-  indexNot = ['379', '657', '1033', '1124', '1154', '1164', '1201', '1289', '2154', '2287', '2428']
+    indexNot = ['379', '657', '1033', '1124', '1154', '1164', '1201', '1289', '2154', '2287', '2428']
 
   # Load labels 
-  with open("data/dataset2/label_train.csv", mode='r') as infile:
-    reader = csv.reader(infile, delimiter = ";")
-    lbls   = {rows[0]:rows[1] for rows in reader}
+    with open("../data/dataset2/label_train.csv", mode='r') as infile:
+        reader = csv.reader(infile, delimiter = ";")
+        lbls   = {rows[0]:rows[1] for rows in reader}
 
   # Remove unwanted images
-  for k in indexNot:
-      lbls.pop(k, None)
+    for k in indexNot:
+        lbls.pop(k, None)
 
 
-  print("Separating train and test sets")
-  random.seed(1)
-  subset = random.sample(range(8967), 896 ) # 3 * 2989 = 8967 total input size
+    print("Separating train and test sets")
+    random.seed(1)
+    subset = random.sample(range(8967), 896 ) # 3 * 2989 = 8967 total input size
 
-  train_data = np.delete(train_all, subset, 0)
-  eval_data = train_all[subset,]
+    train_data = np.delete(train_all, subset, 0)
+    eval_data = train_all[subset,]
   
-  print("Train data dimension:", train_data.shape)
-  print("Test data dimension:", eval_data.shape)
-
-  # Put labels in array format
-  labels_load = np.array(list(lbls.values()))
-  labels_all  = np.concatenate( (labels_load, labels_load, labels_load) )
+    print("Train data dimension:", train_data.shape)
+    print("Test data dimension:", eval_data.shape)
+    #Put labels in array format
+    labels_load = np.array(list(lbls.values()))
+    labels_all  = np.concatenate( (labels_load, labels_load, labels_load) )
 
   
-  train_labels = np.delete(labels_all, subset, 0)
-  eval_labels = labels_all[subset,]
+    train_labels = np.delete(labels_all, subset, 0)
+    eval_labels = labels_all[subset,]
   
-  print("Train labels dimension:", train_labels.shape)
-  print("Test labels dimension:", eval_labels.shape)
-
+    print("Train labels dimension:", train_labels.shape)
+    print("Test labels dimension:", eval_labels.shape)
 
 #   # print( images.shape )
 #   #plt.imshow(np.array(a1[1,:,:,0]), cmap='gray')
@@ -192,14 +190,13 @@ def main(unused_argv):
   ############################################
   ## Run session
   ############################################
-  with tf.Graph().as_default():
-    
-    with tf.Session() as sess:
-
-         train_labels = train_labels.astype( np.int )
-         eval_labels = eval_labels.astype( np.int )
-         batch_size   = 48
-         max_iter     = 1000
+    with tf.Graph().as_default():
+        
+        with tf.Session() as sess:
+            train_labels = train_labels.astype( np.int )
+            eval_labels = eval_labels.astype( np.int )
+            batch_size   = 48
+            max_iter     = 1000
 
 
 #         # instatiate Network
@@ -252,17 +249,17 @@ def main(unused_argv):
         ##
         
         # load trained model
-         net = nets.CnnData2( sess, filterSizeConv1 = 3, nFiltersConv1 = 32, 
+            net = nets.CnnData2( sess, filterSizeConv1 = 3, nFiltersConv1 = 32, 
                              filterSizeConv2 = 3, nFiltersConv2 = 32, filterSizeConv3 = 3, 
                              nFiltersConv3 = 64, fcUnits = 256  ) 
          
-         net.netLoader( "./tmp/DMF/cnnData2" )
+            net.netLoader( "./tmp/DMF/cnnData2" )
 
-         print( "loaded model" )
+            print( "loaded model" )
 
 
          # instantiate deconv net
-         DeconvNet = net.createDeconvNet( eval_data[:100,], eval_labels[:100] )
+            DeconvNet = net.createDeconvNet( eval_data[:100,], eval_labels[:100] )
 
 #         print( "\nDimension of input data")
 #         print( train_data.shape)
@@ -284,11 +281,11 @@ def main(unused_argv):
 #         print( dec3.shape )
 #         print( dec3 )
                  
-         conv1, conv2, conv3 = net.getConvs()
-        
-         #Images within Layers (Aman)
-        
-         images_within_layer1 = net.activate( conv1, eval_data[0,], sess)
+         #conv1, conv2, conv3 = net.getConvs()
+        #
+         ##Images within Layers (Aman)
+        #
+         #images_within_layer1 = net.activate( conv1, eval_data[0,], sess)
          #np.save("tmp/DMF/FilterProjectionDMF_Layer1.npy", images_within_layer1)
         
          #images_within_layer2 = net.activate( conv2, eval_data[0,], sess)
@@ -304,42 +301,60 @@ def main(unused_argv):
 #        
 #        #Activations part----------------------------
 #
-#         np.save("tmp/DMF/eval_data_dmf.npy", eval_data[:100,])
-#         
-#         print("\n")
-#         a1, b1, c1 = DeconvNet.displayFeatures1(eval_data[:100,], eval_labels[:100], n_best = 10, k = 9)
-#         np.save("tmp/DMF/ActivationsDMF_Layer1.npy", a1)
-#         np.save("tmp/DMF/BestImagesDMF_Layer1.npy", b1)
-#         np.save("tmp/DMF/RandomFiltersIndex_Layer1.npy", c1)
-#         print(a1.shape)
+            np.save("tmp/DMF/eval_data_dmf.npy", eval_data[:100,])
+             
+            activations_feature1,best_feature1, filter_index_feature1=DeconvNet.bestActivation1(eval_data[:100],eval_labels[:100])
+            print(best_feature1)
+            np.save("tmp/DMF/ActivationsDMF_Layer1_Features.npy", activations_feature1)
+            np.save("tmp/DMF/BestImagesDMF_Layer1_Features.npy", best_feature1)
+            np.save("tmp/DMF/RandomFiltersIndexDMF_Layer1_Features.npy", filter_index_feature1)
+            print("\n")
+            
+            activations_feature2,best_feature2,filter_index_feature2=DeconvNet.bestActivation2(eval_data[:100],eval_labels[:100])
+            print(best_feature2)
+            np.save("tmp/DMF/ActivationsDMF_Layer2_Features.npy", activations_feature2)
+            np.save("tmp/DMF/BestImagesDMF_Layer2_Features.npy", best_feature2)
+            np.save("tmp/DMF/RandomFiltersIndexDMF_Layer2_Features.npy", filter_index_feature2)
+            print("\n")
+            
+            activations_feature3,best_feature3, filter_index_feature3 =DeconvNet.bestActivation3(eval_data[:100],eval_labels[:100])
+            print(best_feature3)
+            np.save("tmp/DMF/ActivationsDMF_Layer3_Features.npy", activations_feature3)
+            np.save("tmp/DMF/BestImagesDMF_Layer3_Features.npy", best_feature3)
+            np.save("tmp/DMF/RandomFiltersIndexDMF_Layer3_Features.npy", filter_index_feature3)
+            print("\n")
+            a1, b1, c1 = DeconvNet.displayFeatures1(eval_data[:100,], eval_labels[:100], n_best = 3, k = 3)
+            np.save("tmp/DMF/ActivationsDMF_Layer1.npy", a1)
+            np.save("tmp/DMF/BestImagesDMF_Layer1.npy", b1)
+            np.save("tmp/DMF/RandomFiltersIndex_Layer1.npy", c1)
+            print(b1)
+        
+            print("\n")
+            a2, b2, c2 = DeconvNet.displayFeatures2(eval_data[:100,], eval_labels[:100], n_best = 3, k = 3)
+            np.save("tmp/DMF/ActivationsDMF_Layer2.npy", a2)
+            np.save("tmp/DMF/BestImagesDMF_Layer2.npy", b2)
+            np.save("tmp/DMF/RandomFiltersIndex_Layer2.npy", c2)
+            print(b2)  
+        
+            print("\n")
+            a3, b3, c3 = DeconvNet.displayFeatures2(eval_data[:100,], eval_labels[:100], n_best = 3, k = 3)
+            np.save("tmp/DMF/ActivationsDMF_Layer3.npy", a3)
+            np.save("tmp/DMF/BestImagesDMF_Layer3.npy", b3)
+            np.save("tmp/DMF/RandomFiltersIndex_Layer3.npy", c3)
+            print(b3)     
 #        
-#         print("\n")
-#         a2, b2, c2 = DeconvNet.displayFeatures2(eval_data[:100,], eval_labels[:100], n_best = 10, k = 9)
-#         np.save("tmp/DMF/ActivationsDMF_Layer2.npy", a2)
-#         np.save("tmp/DMF/BestImagesDMF_Layer2.npy", b2)
-#         np.save("tmp/DMF/RandomFiltersIndex_Layer2.npy", c2)
-#         print(a2.shape)  
-#        
-#         print("\n")
-#         a3, b3, c3 = DeconvNet.displayFeatures2(eval_data[:100,], eval_labels[:100], n_best = 10, k = 9)
-#         np.save("tmp/DMF/ActivationsDMF_Layer3.npy", a3)
-#         np.save("tmp/DMF/BestImagesDMF_Layer3.npy", b3)
-#         np.save("tmp/DMF/RandomFiltersIndex_Layer3.npy", c3)
-#         print(a3.shape)     
-#        
-#         #Weights part--------------------------------
-#        
-#         w1_t, w2_t, w3_t = net.getWeights()
-#        
-#         w1 = w1_t.eval()
-#         np.save("tmp/DMF/WeightDMF_1.npy", w1)
-#        
-#         w2 = w2_t.eval()
-#         np.save("tmp/DMF/WeightDMF_2.npy", w2)
-#        
-#         w3 = w3_t.eval()
-#         np.save("tmp/DMF/WeightDMF_3.npy", w3)
-
+         #Weights part--------------------------------
+        
+            w1_t, w2_t, w3_t = net.getWeights()
+        
+            w1 = w1_t.eval()
+            np.save("tmp/DMF/WeightDMF_1.npy", w1)
+        
+            w2 = w2_t.eval()
+            np.save("tmp/DMF/WeightDMF_2.npy", w2)
+        
+            w3 = w3_t.eval()
+            np.save("tmp/DMF/WeightDMF_3.npy", w3)
          # plt.imshow(np.array1(train_data[1,:,:,0]), cmap='gray')
          # plt.show()
 
